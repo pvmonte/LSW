883d26d9eb15efd686d0039b2947ac438465c260
PauloMonte <paulovdecastro@gmail.com>
2023-01-15 23:24:21 -0300

    [Gameplay/UI] Inventory Window now switch between canvases

---
+++ Assets/Scripts/Inventory/CanvasUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CanvasUI : MonoBehaviour
+{
+    [SerializeField] protected RectTransform inventoryWindowParent;
+    [SerializeField] protected InventoryWindow window;
+    protected RectTransform windowRctTransform;
+    [SerializeField] protected Vector3 windowPosition = new Vector3(0, -30, 0);
+
+    protected void OnEnable()
+    {
+        window.transform.SetParent(inventoryWindowParent);
+
+        if (windowRctTransform == null)
+            windowRctTransform = window.GetComponent<RectTransform>();
+
+        windowRctTransform.anchoredPosition = windowPosition;
+    }
+}
---
+++ Assets/Scripts/Inventory/InventoryCanvas.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class InventoryCanvas : CanvasUI
+{
+    
+}
